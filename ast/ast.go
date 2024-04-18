71a39b6cade8114f0a15155c24fef1d100b80466
ethan <enel@uscs.edu>
2024-04-17 19:24:28 -0700

    Did some more things

--- ast/ast.go
+++ ast/ast.go
@@ -3,6 +3,8 @@ package ast
 import (
 	"bytes"
 	"monkey/token"
+	"runtime/trace"
+	"strings"
 )
 
 type Node interface {
@@ -215,4 +217,55 @@ func (bs *BlockStatement) String() string {
 	}
 
 	return out.String()
+}
+
+type FunctionLiteral struct {
+	Token      token.Token
+	Parameters []*Identifier
+	Body       *BlockStatement
+}
+
+func (fl *FunctionLiteral) expressionNode()      {}
+func (fl *FunctionLiteral) TokenLiteral() string { return fl.Token.Literal }
+func (fl *FunctionLiteral) String() string {
+	var out bytes.Buffer
+
+	params := []string{}
+
+	for _, p := range fl.Parameters {
+		params = append(params, p.String())
+	}
+
+	out.WriteString(fl.TokenLiteral())
+	out.WriteString("(")
+	out.WriteString(strings.Join(params, ", "))
+	out.WriteString(") ")
+	out.WriteString(fl.Body.String())
+
+	return out.String()
+}
+
+type CallExpressions struct {
+	Token     token.Token
+	Function  Expression
+	Arguments []Expression
+}
+
+func (ce *CallExpressions) expressionNode() {}
+func (ce *CallExpressions) TokenLiteral() string { return ce.Token.Literal }
+func (ce *CallExpressions) String() string {
+    var out bytes.Buffer
+
+    args := []string{}
+
+    for _, a := range ce.Arguments {
+        args = append(args, a.String())
+    }
+
+    out.WriteString(ce.Function.String())
+    out.WriteString("(")
+    out.WriteString(strings.Join(args, ", "))
+    out.WriteString(")")
+
+    return out.String()
 }
