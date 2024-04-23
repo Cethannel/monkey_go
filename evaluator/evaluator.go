a5b742e812eb560e8756b7d59e9910b86d114ab6
ethan <enel@uscs.edu>
2024-04-23 16:18:19 -0700

    Added arrays

--- evaluator/evaluator.go
+++ evaluator/evaluator.go
@@ -80,12 +80,30 @@ func Eval(node ast.Node, env *object.Environment) object.Object {
 		if isError(function) {
 			return function
 		}
-		args := evalExpression(node.Arguments, env)
+		args := evalExpressions(node.Arguments, env)
 		if len(args) == 1 && isError(args[0]) {
 			return args[0]
 		}
 
 		return applyFunction(function, args)
+
+	case *ast.ArrayLiteral:
+		elements := evalExpressions(node.Elements, env)
+		if len(elements) == 1 && isError(elements[0]) {
+			return elements[0]
+		}
+		return &object.Array{Elements: elements}
+
+	case *ast.IndexExpression:
+		left := Eval(node.Left, env)
+		if isError(left) {
+			return left
+		}
+		index := Eval(node.Index, env)
+		if isError(index) {
+			return index
+		}
+		return evalIndexExpression(left, index)
 	}
 
 	return nil
@@ -266,7 +284,7 @@ func evalIdentifier(
 	return newError("identifier not found: " + node.Value)
 }
 
-func evalExpression(
+func evalExpressions(
 	exps []ast.Expression,
 	env *object.Environment,
 ) []object.Object {
@@ -292,7 +310,7 @@ func applyFunction(fn object.Object, args []object.Object) object.Object {
 	case *object.Buitin:
 		return fn.Fn(args...)
 	default:
-        return newError("not a function: %s", fn.Type())
+		return newError("not a function: %s", fn.Type())
 	}
 }
 
@@ -327,4 +345,25 @@ func evalStringInfixExpression(operator string,
 	rightVal := right.(*object.String).Value
 
 	return &object.String{Value: leftVal + rightVal}
+}
+
+func evalIndexExpression(left, index object.Object) object.Object {
+	switch {
+	case left.Type() == object.ARRAY_OBJ && index.Type() == object.INTEGER_OBJ:
+		return evalArrayIndexExpression(left, index)
+	default:
+		return newError("index operator not supported: %s", left.Type())
+	}
+}
+
+func evalArrayIndexExpression(array, index object.Object) object.Object {
+    arrayObject := array.(*object.Array)
+    idx := index.(*object.Integer).Value
+    max := int64(len(arrayObject.Elements) - 1)
+
+    if idx < 0 || idx > max {
+        return NULL
+    }
+
+    return arrayObject.Elements[idx]
 }
--- object/object.go
+++ object/object.go
@@ -16,6 +16,7 @@ const (
 	FUNCTION_OBJ     = "FUNCTION"
 	STRING_OBJ       = "STRING"
 	BUILTIN_OBJ      = "BUILTIN"
+	ARRAY_OBJ        = "ARRAY"
 )
 
 type ObjectType string
@@ -98,3 +99,23 @@ type Buitin struct {
 
 func (b *Buitin) Type() ObjectType { return BUILTIN_OBJ }
 func (b *Buitin) Inspect() string  { return "builtin function" }
+
+type Array struct {
+	Elements []Object
+}
+
+func (ao *Array) Type() ObjectType { return ARRAY_OBJ }
+func (ao *Array) Inspect() string {
+	var out bytes.Buffer
+
+	elements := []string{}
+	for _, e := range ao.Elements {
+		elements = append(elements, e.Inspect())
+	}
+
+	out.WriteString("[")
+	out.WriteString(strings.Join(elements, ", "))
+	out.WriteString("]")
+
+	return out.String()
+}
